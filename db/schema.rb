97f1b7a55ed00f7e7b9ab5f6ca5f99a98d84d55e
Vinson Koo <vinsonkoo@gmail.com>
2015-12-04 19:43:42 -0500

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -14,14 +14,14 @@
 ActiveRecord::Schema.define(version: 20151204034653) do
 
   create_table "active_admin_comments", force: :cascade do |t|
-    t.string   "namespace",     limit: 255
+    t.string   "namespace"
     t.text     "body"
-    t.string   "resource_id",   limit: 255, null: false
-    t.string   "resource_type", limit: 255, null: false
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
     t.integer  "author_id"
-    t.string   "author_type",   limit: 255
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.string   "author_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
@@ -29,18 +29,18 @@ ActiveRecord::Schema.define(version: 20151204034653) do
   add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
 
   create_table "admin_users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
@@ -48,18 +48,17 @@ ActiveRecord::Schema.define(version: 20151204034653) do
 
   create_table "chats", force: :cascade do |t|
     t.text     "chat"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "pact_id"
-    t.boolean  "media"
   end
 
   add_index "chats", ["pact_id"], name: "index_pact_id_2"
 
   create_table "goals", force: :cascade do |t|
     t.integer  "goal"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "pact_id"
     t.integer  "week_id"
@@ -72,12 +71,12 @@ ActiveRecord::Schema.define(version: 20151204034653) do
 
   create_table "messages", force: :cascade do |t|
     t.text     "message"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "pact_id"
     t.datetime "date_sent"
-    t.string   "photo_url",     limit: 255
+    t.string   "photo_url"
     t.boolean  "media"
     t.string   "image"
     t.string   "video"
@@ -94,10 +93,10 @@ ActiveRecord::Schema.define(version: 20151204034653) do
   add_index "messages", ["user_id"], name: "index_user_id"
 
   create_table "pact_user_relations", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "pact_id"
-    t.integer  "user_id"
+    t.text     "user_id"
   end
 
   add_index "pact_user_relations", ["pact_id", "user_id"], name: "index_pact_user_relations_on_pact_id_and_user_id", unique: true
@@ -105,12 +104,12 @@ ActiveRecord::Schema.define(version: 20151204034653) do
   add_index "pact_user_relations", ["user_id"], name: "index_pact_user_relations_on_user_id"
 
   create_table "pacts", force: :cascade do |t|
-    t.string   "pact_name",  limit: 255
+    t.string   "pact_name"
     t.date     "start_date"
     t.date     "end_date"
     t.boolean  "is_active"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.float    "penalty"
   end
 
@@ -128,8 +127,8 @@ ActiveRecord::Schema.define(version: 20151204034653) do
   create_table "penalties", force: :cascade do |t|
     t.integer  "goal",       default: 0,   null: false
     t.float    "penalty",    default: 0.0, null: false
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "pact_id"
   end
 
@@ -137,10 +136,10 @@ ActiveRecord::Schema.define(version: 20151204034653) do
   add_index "penalties", ["pact_id"], name: "index_pact_id"
 
   create_table "photos", force: :cascade do |t|
-    t.string   "photo_url",  limit: 255
+    t.string   "photo_url"
     t.text     "notes"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "message_id"
   end
 
@@ -171,17 +170,18 @@ ActiveRecord::Schema.define(version: 20151204034653) do
     t.date     "start_date"
     t.date     "end_date"
     t.integer  "week_number"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "pact_id"
   end
 
+  add_index "weeks", ["pact_id", "week_number"], name: "index_weeks_on_pact_id_and_week_number", unique: true
   add_index "weeks", ["pact_id"], name: "index_pact_id_3"
 
   create_table "workout_types", force: :cascade do |t|
-    t.string   "workout_type", limit: 255
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "workout_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "workout_id"
   end
 
@@ -190,15 +190,15 @@ ActiveRecord::Schema.define(version: 20151204034653) do
 
   create_table "workouts", force: :cascade do |t|
     t.float    "distance"
-    t.string   "pace",                limit: 255
-    t.string   "duration",            limit: 255
-    t.string   "video1",              limit: 255
-    t.string   "video2",              limit: 255
-    t.string   "workout_name",        limit: 255
+    t.string   "pace"
+    t.string   "duration"
+    t.string   "video1"
+    t.string   "video2"
+    t.string   "workout_name"
     t.text     "workout_description"
     t.boolean  "is_makeup_workout"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "photo_id"
     t.integer  "week_id"
