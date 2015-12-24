09b1fa5e7e39f00dc594ac764fd7745690de91a3
Cynthia Koo <cyn.koo@gmail.com>
2015-12-24 02:52:46 -0500

    Style pact creation forms

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -13,4 +13,11 @@ class Goal < ActiveRecord::Base
   # deprecated in rails 4
 	# Makes it so that you can edit these database columns via ActiveAdmin and forms
   # attr_accessible :goal, :user_id, :pact_id, :week_id
+
+	def get_user(user_id)
+		User.find_by(id: user_id)
+	end
+
+
+
 end
