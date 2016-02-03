9804f5eec804903ca6b0956dd28c992cdf696a03
Vinson Koo <vinsonkoo@gmail.com>
2016-02-03 18:41:50 -0500

    typo in a migration file

--- db/migrate/20160102205317_remove_index_for_workouts.rb
+++ db/migrate/20160102205317_remove_index_for_workouts.rb
@@ -1,6 +1,6 @@
 class RemoveIndexForWorkouts < ActiveRecord::Migration
   def change
     remove_index(:workouts, :name => 'index_user_id_3')
-    remove_index(:workouts, :name => 'index_workouts_on_user_id_and_photo_id')
+    remove_index(:workouts, :name => 'index_workout_types_on_workout_id_and_workout_type')
   end
 end
