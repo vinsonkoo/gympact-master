6cde7f9f0ef292142ccc53ad4b5f5679a73b32b9
Vinson Koo <vinsonkoo@gmail.com>
2016-02-03 18:54:55 -0500

    migration fix

--- db/migrate/20160102205317_remove_index_for_workouts.rb
+++ db/migrate/20160102205317_remove_index_for_workouts.rb
@@ -1,6 +1,5 @@
 class RemoveIndexForWorkouts < ActiveRecord::Migration
   def change
     remove_index(:workouts, :name => 'index_user_id_3')
-    remove_index(:workouts, :name => 'index_workout_types_on_workout_id_and_workout_type')
   end
 end
