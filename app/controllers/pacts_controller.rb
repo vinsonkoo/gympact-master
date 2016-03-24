78634c4b15de2f808c41ab81415ac27eea1ab49f
Vinson Koo <vinsonkoo@gmail.com>
2016-03-24 17:30:47 -0400

    pact weeks view updated to show penalty total

--- app/controllers/pacts_controller.rb
+++ app/controllers/pacts_controller.rb
@@ -141,7 +141,7 @@ class PactsController < ApplicationController
       :start_date, 
       penalties_attributes:[:id, :pact_id, :penalty, :goal_days], 
       user_ids:[], 
-      goals_attributes:[:id, :pact_id, :goal, :user_id, :week_id],
+      goals_attributes:[:id, :pact_id, :goal_days, :user_id, :week_id],
       messages_attributes:[:id, :pact_id, :user_id, :message, :date_sent, :photo_url, :media, :image, :video, :date, :time, :msg_date_time, :sender, :user, :week_id],
       workouts_attributes:[:id, :distance, :duration, :is_makeup_workout, :pace, :video1, :video2, :workout_description, :workout_name, :sent, :photo_id, :user_id, :week_id, :message_id],
       payments_attributes:[:id, :owed, :paid, :user_id, :pact_id]
